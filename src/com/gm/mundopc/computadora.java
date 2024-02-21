a5c0ce5737b91ce0096ede313673f05125d89a7f
LATITUDE E5440 <LATITUDE E5440@192.168.95.1>
2024-02-20 20:30:11 -0600

    esqueleto inicial3

--- src/com/gm/mundopc/computadora.java
+++ src/com/gm/mundopc/computadora.java
@@ -1,13 +1,59 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.gm.mundopc;
 
-/**
- *
- * @author LATITUDE E5440
- */
 public class computadora {
+
+	private int idComputadora;
+	private String nombre;
+	private Monitor monitor;
+	private Teclado teclado;
+	private Raton raton;
+
+	private computadora() {
+
+	}
+
+	public computadora(String nombre, Monitor monitor, Teclado teclado, Raton raton) {
+		this();
+		this.nombre = nombre;
+		this.monitor = monitor;
+		this.teclado = teclado;
+		this.raton = raton;
+	}
 	
+
+	//getter nombre
+	public String Getnombre() {
+		return nombre;
+	}
+
+	//setter nombre
+	public void Setnombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	//getter idComputadora
+	public int GETidComputadora() {
+		return idComputadora;
+	}
+
+	//setter idComputadora
+	public void SETidComputadora(int idComputadora) {
+		this.idComputadora = idComputadora;
+	}
+
+	
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("computadora{");
+		sb.append("idComputadora=").append(idComputadora);
+		sb.append(", nombre=").append(nombre);
+		sb.append(", monitor=").append(monitor);
+		sb.append(", teclado=").append(teclado);
+		sb.append(", raton=").append(raton);
+		sb.append('}');
+		return sb.toString();
+	}
+
 }
