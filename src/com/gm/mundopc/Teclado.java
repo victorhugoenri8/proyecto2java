f76651ed1caa89e15b792f44af2aa2100d10d4a7
LATITUDE E5440 <LATITUDE E5440@192.168.95.1>
2024-02-22 17:31:29 -0600

    final resuelto3: version final

--- src/com/gm/mundopc/Teclado.java
+++ src/com/gm/mundopc/Teclado.java
@@ -2,23 +2,30 @@ package com.gm.mundopc;
 
 public class Teclado extends DispositivoEntrada {
 
-	private int idTeclado;
-	private int contadorTeclados;
+	private final int idTeclado;
+	private static int contadorTeclados;
 
 	//metodos
-	public Teclado(int idTeclado, int contadorTeclados) {
-		this.idTeclado = idTeclado;
-		this.contadorTeclados = contadorTeclados;
+	public Teclado(String tipoEntrada, String marca) {
+		super(tipoEntrada, marca);
+		this.idTeclado = ++Teclado.contadorTeclados;
 	}
+
+//	@Override
+//	public String toString() {
+//		StringBuilder sb = new StringBuilder();
+//		sb.append("Teclado{");
+//		sb.append("idTeclado=").append(idTeclado);
+//		sb.append(", contadorTeclados=").append(contadorTeclados);
+//		sb.append('}');
+//		return sb.toString();
+//	}
 
 	@Override
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("Teclado{");
-		sb.append("idTeclado=").append(idTeclado);
-		sb.append(", contadorTeclados=").append(contadorTeclados);
-		sb.append('}');
-		return sb.toString();
+		return "Teclado{" + "idTeclado=" + idTeclado + ", " + super.toString() + '}';
 	}
+	
+	
 
 }
