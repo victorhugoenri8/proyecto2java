b6df7fb9e2378f4e6867312a66f9d205168a6596
LATITUDE E5440 <LATITUDE E5440@192.168.95.1>
2024-02-22 17:27:06 -0600

    final resuelto3: version final

--- src/com/gm/mundopc/Raton.java
+++ src/com/gm/mundopc/Raton.java
@@ -2,18 +2,30 @@ package com.gm.mundopc;
 
 public class Raton extends DispositivoEntrada {
 
-	private int idRaton;
-	private int contadorRatones;
+	private final int idRaton;
+	private static int contadorRatones;
+	
+	public Raton (String tipoEntrada, String marca) {
+		super(tipoEntrada, marca);
+		this.idRaton = ++Raton.contadorRatones;
+	}
 
 	//metodos
-	public Raton(int idRaton, int contadorRatones) {
-		this.idRaton = idRaton;
-		this.contadorRatones = contadorRatones;
-	}
+//	public Raton(int idRaton, int contadorRatones) {
+//		this.idRaton = idRaton;
+//		this.contadorRatones = contadorRatones;
+//	}
+
+//	@Override
+//	public String toString() {
+//		return "Raton{" + "idRaton=" + idRaton + ", contadorRatones=" + contadorRatones + '}';
+//	}
 
 	@Override
 	public String toString() {
-		return "Raton{" + "idRaton=" + idRaton + ", contadorRatones=" + contadorRatones + '}';
+		return "Raton{" + "idRaton=" + idRaton + ", " + super.toString() + '}';
 	}
 
+	
+	
 }
